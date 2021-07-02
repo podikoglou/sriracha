3f8aff9a1bdb08be3f11e93b187b44483779c592
Alex <>
2021-07-02 18:39:17 -0400

    document application.py

--- sriracha/application.py
+++ sriracha/application.py
@@ -1,6 +1,12 @@
 from types import ModuleType
 
 class Application:
+    """
+    an application is an application.
+
+    note: only one application can be created per runtime! (this is not very
+    strict, i'm not gonna tell your parents, it's just gonna fuck up)
+    """
 
     def __init__(self, settings_module: ModuleType):
         # hacky way to get all variables that are not weird python variables
