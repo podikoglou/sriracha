c78d8875a9219f78ea3ebe67ddfccd95cd071737
Alex <>
2021-07-02 20:35:55 -0400

    change the way we get settings

--- sriracha/application.py
+++ sriracha/application.py
@@ -1,4 +1,5 @@
 from types import ModuleType
+from . import utils
 
 class Application:
     """
@@ -10,7 +11,4 @@ class Application:
 
     def __init__(self, settings_module: ModuleType):
         # hacky way to get all variables that are not weird python variables
-        self.settings = [(key, value) for key, value in vars(settings_module).keys() if not key in utils.IGNORE_VARS]
-
-    def __call__(a, b):
-        print('yessir')
+        self.settings = {key: value for key, value in vars(settings_module).items() if not key in utils.IGNORE_VARS}
