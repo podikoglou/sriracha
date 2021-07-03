00132bb87e117b2eba5025d9210594af580afdd9
Alex <>
2021-07-02 22:12:28 -0400

    crappily implement render_view

--- sriracha/view.py
+++ sriracha/view.py
@@ -1,2 +1,15 @@
-def render_view():
-    pass
+def render_view(name, model):
+    """
+    renders a view with a model using liquid i think
+
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    CODE BELOW IS A SHITTY IMPLEMENTATION
+    """
+
+    with open('views/' + name + '.html', 'r') as file:
+        return file.read()
