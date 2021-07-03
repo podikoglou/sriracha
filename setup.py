0713d87662f315d3c890f0ae71548a08489733d8
Alex <>
2021-07-03 09:00:24 +0300

    merge with sri and prepare for publishing to pypi

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name='sriracha',
     version='0.1',
-    description='',
+    description='MVC Web Framework',
 
     author='pc',
     author_email='crunch@disroot.org',
 
     packages=find_packages(),
-    install_requires=['python-liquid'],
+
+    install_requires=['python-liquid', 'click', 'GitPython'],
+
+    entry_points={
+        'console_scripts': [
+            'sri=sri:main'
+        ]
+    },
 
     license='GPL',
     license_file='LICENSE'
